5d85b7406e01e7f56f988bf4a7187a986384c725
Gordon <qihao.ou@connect.ust.hk>
2020-07-19 15:41:07 +0800

    VS-MSBuild.

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# VS-MSBuild
+Invoke the latest version of MSBuild installed with Visual Studio.
+
+# Example
+```
+dotnet tool install --global VS-MSBuild
+```
+```
+vs-msbuild
+```
---
+++ VS-MSBuild/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.DotNet.Cli.Utils;
+
+namespace VS_MSBuild
+{
+    class Program
+    {
+        private static string[] Run(string commandName, params string[] args)
+        {
+            var lines = new List<string>();
+            var command = Command
+                .Create(commandName, args);
+            Console.WriteLine($"{command.CommandName} {command.CommandArgs}");
+            var result = command.OnOutputLine(lines.Add).Execute();
+            if (result.ExitCode != 0)
+            {
+                Environment.Exit(result.ExitCode);
+            }
+            return lines.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            string vswherePath = Path.Combine(
+                Environment.GetEnvironmentVariable("ProgramFiles(x86)"),
+                "Microsoft Visual Studio",
+                "Installer",
+                "vswhere.exe");
+            if (!File.Exists(vswherePath))
+            {
+                Console.WriteLine($"vswhere not found: {vswherePath}.");
+            }
+            var lines = Run(vswherePath,
+                "-latest",
+                "-requires", "Microsoft.Component.MSBuild",
+                "-find", @"MSBuild\**\Bin\MSBuild.exe");
+            if (lines.Length == 0)
+            {
+                Console.WriteLine("MSBuild not found.");
+            }
+            Run(lines[0], args);
+        }
+    }
+}
